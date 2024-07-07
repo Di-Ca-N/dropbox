18a530fee6f6d4e19254217e63a0f96770d4bceb
Diego Cardoso Nunes <diegocar.nunes@gmail.com>
2024-07-07 18:48:09 -0300

    Fixed DeviceManager bug during notification

--- server/DeviceManager.cpp
+++ server/DeviceManager.cpp
@@ -6,6 +6,7 @@ DeviceManager::DeviceManager(std::string username) {
 }
 
 Device& DeviceManager::registerDevice() {
+    // Locking to ensure consistency when registering a device
     std::lock_guard<std::mutex> lock(mutex);
     Device device = {
         .id=deviceId,
@@ -19,18 +20,20 @@ Device& DeviceManager::registerDevice() {
 }
 
 Device DeviceManager::getDevice(int deviceId) {
+    // Locking to ensure that the device does not change during read
     std::lock_guard<std::mutex> lock(mutex);
     return devices[deviceId];
 }
 
 void DeviceManager::connectDevice(int deviceId) {
+    // Locking to ensure consistency when adding a device connection
     std::lock_guard<std::mutex> lock(mutex);
     Device& device = devices[deviceId];
     device.numConnections++;
 }
 
 void DeviceManager::disconnectDevice(int deviceId) {
-    // Locking to ensure consistency when deleting a device
+    // Locking to ensure consistency when disconnecting a device
     std::lock_guard<std::mutex> lock(mutex);
     Device& device = devices[deviceId];
 
@@ -46,12 +49,16 @@ void DeviceManager::disconnectDevice(int deviceId) {
 }
 
 void DeviceManager::notifyAllDevices(FileOperation op) {
+    // Locking to avoid devices being added or removed during the notification
+    std::lock_guard<std::mutex> lock(mutex);
     for (auto &[id, device] : devices) {
         device.queue->push(op);
     }
 }
 
 void DeviceManager::notifyOtherDevices(FileOperation op, int sourceDeviceId) {
+    // Locking to avoid devices being added or removed during the notification
+    std::lock_guard<std::mutex> lock(mutex); 
     for (auto &[id, device] : devices) {
         if (id != sourceDeviceId) {
             device.queue->push(op);
